dd32a6c258af6c60d3baef747405f73a1b79de11
Sam <klioop2@gmail.com>
2021-05-09 13:43:58 +0900

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by klioop on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
